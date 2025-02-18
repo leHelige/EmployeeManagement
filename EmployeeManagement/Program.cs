8a784b5da304201be2a3b821e6b4285b77ab78f4
leHelige <192903879+leHelige@users.noreply.github.com>
2025-02-18 13:49:46 -0300

    Add project files.

---
+++ EmployeeManagement/Program.cs
@@ -0,0 +1,36 @@
+using EmployeeManagement.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+
+namespace EmployeeManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<AppDbContext>(
+                options => options.UseInMemoryDatabase("EmployeeDb")
+                );
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("MyCors", builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200")
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
+
+            var app = builder.Build();
+
+            app.UseCors();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
