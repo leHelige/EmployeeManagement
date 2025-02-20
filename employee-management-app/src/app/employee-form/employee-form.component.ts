8d6fe358f4030fbed38088a9f1d448f098ded48b
leHelige <192903879+leHelige@users.noreply.github.com>
2025-02-20 17:24:07 -0300

    Define the routes, create the navigation menu and create the employee forms

---
+++ employee-management-app/src/app/employee-form/employee-form.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import type { Employee } from '../../models/employee';
+
+@Component({
+  selector: 'app-employee-form',
+  imports: [FormsModule],
+  templateUrl: './employee-form.component.html',
+  styleUrl: './employee-form.component.css'
+})
+export class EmployeeFormComponent {
+
+  employee: Employee = {
+    id: 0,
+    firstName: '',
+    lastName: '',
+    phone: '',
+    email: '',
+    position: ''
+  }
+
+  onSubmit() : void {
+    console.log(this.employee);
+  }
+}
